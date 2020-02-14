15d37f63cbf12b789a6cd2e3013051f3e307f6a0
viraltaco <7462994+ViralTaco@users.noreply.github.com>
2020-02-14 02:28:50 +0100

    Complete redesign of the LogView struct.

--- pillz/View/LogView.swift
+++ pillz/View/LogView.swift
@@ -9,30 +9,50 @@
 import Foundation
 import Rainbow
 
+infix operator <->
+fileprivate extension String {
+  static func <-> (lhs: String, rhs: String) -> String {
+    let spacing = abs(ViewConstants.spacing - (lhs.count + rhs.count))
+    return lhs + String(Array(repeating: " ", count: spacing)) + rhs
+  }
+}
+
+
 struct LogView {
-  fileprivate static var dateFormatter: DateFormatter {
-    let df = DateFormatter()
-    df.dateFormat = "yyyy-MM-dd HH:mm"
-    return df
+  static func doseString(_ dose: Double) -> String {
+    guard dose < 1000.0 else {
+      return "\(dose/1000.0)\(ViewConstants.gram) "
+    }
+    return "\(dose)\(ViewConstants.mg)"
   }
   
-  private static let mg = "mg".lightWhite
-  
-  static func dateToString(_ date: Date = Date()) -> String {
-    return dateFormatter.string(from: date)
+  static func extend(_ str: String, _ length: Int = 60) -> String {
+    guard str.count < length else { return str }
+    return str + String(Array(repeating: " ", count: length - str.count))
   }
   
-  static func print(_ log: Log?) -> Void {
+  static func print(_ log: Log?, lightBackground isAlt: Bool = false) -> Void {
     guard log != nil else { return }
-    let (date, name, dose) = (dateToString(log!.date!), log!.name!, log!.dose!)
-    Swift.print("  \(date)\t\(name)\t\(dose)\(ViewConstants.mg)")
+    let lhs = DateView(for: log!.date!).fullDate
+    let rhs = log!.name! <-> doseString(log!.dose!)
+    let line = extend("  \(lhs)      \(rhs)")
+    
+    if isAlt {
+      Swift.print(line.white.onBlack)
+    } else {
+      Swift.print(line)
+    }
   }
   
   static func print(_ logs: [Log]) -> Void {
     CurrentView.separator()
-    for log in logs {
-      LogView.print(log)
+    for i in 0..<logs.count {
+      LogView.print(logs[i], lightBackground: i.isOdd)
     }
     CurrentView.separator()
   }
+}
+
+fileprivate extension Int {
+  var isOdd: Bool { self & 0b1 != 0 }
 }
