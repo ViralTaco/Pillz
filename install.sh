5be29fb92d94ef9e86465ed25970f11ba1b38978
viraltaco <7462994+ViralTaco@users.noreply.github.com>
2020-03-06 07:41:27 +0100

    hashbang is now /bin/bash (used to be /bin/sh); New custom function: puts (behaves like printf but adding a newline after each argument); New custom function: does_app_exist (returns hash $@ >/dev/null 2>&1 ); Root check if statement is now a (()) instead of []; Added check if xcodebuild is installed, else print instructions to install it; Overall replaced echo with the new custom command puts; Replaced [] tests with [[]] tests;

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+function does_app_exist() {
+    hash $@ >/dev/null 2>&1
+}
+
+function puts() {
+    for line in "$@"; do
+        printf "$line\n"
+    done
+}
 
 # Constants:
 BUILD_NAME='Pillz'
@@ -11,35 +21,40 @@ WORKSPACE="$BUILD_NAME.xcworkspace"
 SCHEME='pilllog'
 
 # Make sure to have root
-if [ $EUID != 0 ]; then 
-    echo 'You need root to run this script.'
-    sudo "$0" "$@"
+if (( $EUID != 0 )); then 
+    puts "You need root to run this script."
+    sudo "./$0"
     exit $?
 fi
 
-# Compile:
-echo 'Compiling...'
+if ! does_app_exist xcodebuild; then
+    puts "Error: Couldn't find xcodebuild." 
+    puts "You need Xcode installed to run this script."
+    puts "You can download Xcode here: https://apps.apple.com/us/app/xcode/id497799835"
+    exit $?
+fi
+
+puts "Compiling..."
 if xcodebuild -workspace $WORKSPACE -scheme $SCHEME -quiet; then
-    echo 'Done.'
+    puts "Done."
 else # xcodebuild failed.
-    echo 'Could not compile. Try building using Xcode'
+    puts "Could not compile. Try building using Xcode"
+    exit $?
 fi
 
 # Remove current install if it exists:
-if [ -f $PRODUCT_PATH ]; then
-    echo 'Removing previous version...'
+if [[ -f $PRODUCT_PATH ]]; then
+    puts "Removing previous version..."
     rm $PRODUCT_PATH ||: # rm .. or true (don't print error)
-    echo 'Done.'
-fi # [ -f $PRODUCT_PATH ]
+    puts "Done."
+fi # [[ -f $PRODUCT_PATH ]]
     
 # Copy built product to the bin folder
-echo "Copying $PRODUCT_NAME to $PRODUCT_PATH..."
+puts "Copying $PRODUCT_NAME to $PRODUCT_PATH..."
 if cp $BUILD_PATH $PRODUCT_PATH; then
-    echo 'Done.'
-    echo "Application installed. You can run it by typing $PRODUCT_NAME"
+    puts "Done." "Application installed. You can run it by typing $PRODUCT_NAME" 
 else # Couldn't copy file. 
-    echo 'Failed.'
-    echo 'Could not complete installation.'
+    puts "Failed." "Could not copy $PRODUCT_NAME to $PRDODUCT_PATH."
 fi
 
 exit $?
