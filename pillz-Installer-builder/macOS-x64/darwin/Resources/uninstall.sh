5ea2110a750ac97e84bbc2bd3f4596d4768f07e1
viraltaco <7462994+viraltaco@users.noreply.github.com>
2020-07-11 16:42:25 +0200

    Added pillz-Installer-builder

---
+++ pillz-Installer-builder/macOS-x64/darwin/Resources/uninstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#Generate application uninstallers for macOS.
+
+#Parameters
+DATE=`date +%Y-%m-%d`
+TIME=`date +%H:%M:%S`
+LOG_PREFIX="[$DATE $TIME]"
+
+#Functions
+log_info() { 
+    echo "${LOG_PREFIX}[INFO]" $1 
+}
+log_warn() { 
+    echo "${LOG_PREFIX}[WARN]" $1 
+}
+log_error() { 
+    echo "${LOG_PREFIX}[ERROR]" $1 
+}
+
+#Check running user
+(( $EUID != 0 )) && \
+    echo "You need root to run this script." && \
+    exit $(sudo "$0" "$@")
+
+
+echo "Welcome to Application Uninstaller"
+echo "The following packages will be REMOVED:"
+echo "  __PRODUCT__-__VERSION__"
+
+read -p "Continue [y/n]?"
+[[ $REPLY = [yY] ]] || { echo "Cancelled."; exit 1; }
+
+
+#Need to replace these with install preparation script
+VERSION=__VERSION__
+PRODUCT=__PRODUCT__
+
+echo "Uninstalling..."
+# remove link to shorcut file
+find "/usr/local/bin/" -name "__PRODUCT__-__VERSION__" | xargs rm
+if (( "$?" == 0 )); then
+    echo "[1/3] [DONE] Successfully deleted shortcut links"
+else
+    echo "[1/3] [ERROR] Could not delete shortcut links" >&2
+fi
+
+#forget from pkgutil
+pkgutil --forget "org.$PRODUCT.$VERSION" &>/dev/null
+if (( "$?" == 0 )); then
+    echo "[2/3] [DONE] Successfully deleted application informations"
+else
+    echo "[2/3] [ERROR] Could not delete application informations" >&2
+fi
+
+#remove application source distribution
+[[ -e "/Library/${PRODUCT}/${VERSION}" ]] && rm -r "/Library/${PRODUCT}/${VERSION}"
+if (( "$?" == 0 )); then
+    echo "[3/3] [DONE] Successfully deleted application"
+else
+    echo "[3/3] [ERROR] Could not delete application" >&2
+fi
+
+echo "Success."
+exit 0
